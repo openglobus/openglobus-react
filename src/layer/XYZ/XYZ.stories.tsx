763e052ad4239d0d9f5bf5d8a3efcbc7539b8172
Pavel <pvlt.job@gmail.com>
2024-06-11 18:10:19 +0400

    sat layer for xyz story

--- src/layer/XYZ/XYZ.stories.tsx
+++ src/layer/XYZ/XYZ.stories.tsx
@@ -4,6 +4,7 @@ import type {Meta, StoryObj} from '@storybook/react';
 import {Globus, GlobusContextProvider} from "@/Globe";
 import React from "react";
 import {XYZ, XYZProps} from "@/layer/XYZ/XYZ";
+import {utils} from "@openglobus/og";
 
 const meta = {
     title: 'Components/Layer/XYZ',
@@ -15,6 +16,17 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 
+function toQuadKey(x: number, y: number, z: number): string {
+    var index = '';
+    for (var i = z; i > 0; i--) {
+        var b = 0;
+        var mask = 1 << (i - 1);
+        if ((x & mask) !== 0) b++;
+        if ((y & mask) !== 0) b += 2;
+        index += b.toString();
+    }
+    return index;
+}
 export const OpenStreetMap: Story = {
 
     args: {
@@ -31,12 +43,18 @@ export const OpenStreetMap: Story = {
 export const Satellites: Story = {
     args: {
         name: 'sat',
-        url: 'https://ecn.{s}.tiles.virtualearth.net/tiles/a{quad}.jpeg?n=z&g=7146'
+        url: 'https://ecn.{s}.tiles.virtualearth.net/tiles/a{quad}.jpeg?n=z&g=7146',
+        subdomains: ['t0', 't1', 't2', 't3'],
+        urlRewrite: function (s: any, u: string) {
+            console.log(s)
+            return utils.stringTemplate(u, {s: ['t0', 't1', 't2', 't3'][Math.round(Math.random() * 3)], quad: toQuadKey(s.tileX, s.tileY, s.tileZoom)});
+        }
     },
-    render: (args: XYZProps) => <GlobusContextProvider>
-        <Globus atmosphereEnabled={false}>
-            <XYZ {...args} />
-        </Globus>
-    </GlobusContextProvider>
+    render: (args: XYZProps) =>
+        <GlobusContextProvider>
+            <Globus>
+                <XYZ {...args} />
+            </Globus>
+        </GlobusContextProvider>
 }
 
